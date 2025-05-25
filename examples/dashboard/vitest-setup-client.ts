538db99f4978965ba66ea66d559078c6c244573b
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-05-25 23:42:19 +0100

    Fuck you prettier.

--- examples/dashboard/vitest-setup-client.ts
+++ examples/dashboard/vitest-setup-client.ts
@@ -21,9 +21,9 @@ Object.defineProperty(window, 'localStorage', {
 		getItem: vi.fn(),
 		setItem: vi.fn(),
 		removeItem: vi.fn(),
-		clear: vi.fn(),
+		clear: vi.fn()
 	},
-	writable: true,
+	writable: true
 });
 
 // Mock environment variables
